0f8dc9b6f5a401111473251e5df09bf7f2f3eeb9
Ebad Kamil <ebad.kamil@ess.eu>
2022-03-07 15:52:08 +0100

    Add PostGreConnection descriptor

--- connection.py
+++ connection.py
@@ -22,3 +22,38 @@ def init_postgre_connection(dbname, user, password, host, port):
 
 def postgre_connection():
     return _GLOBAL_POSTGRE_CONNECTION
+
+
+def commit_and_close_connections():
+    for connections in _global_connections.values():
+        for ref in connections:
+            c = ref()
+            if c is not None:
+                c.close()
+
+
+class MetaPostGreConnection(type):
+    def __call__(cls, *args, **kw):
+        instance = super().__call__(*args, **kw)
+        name = cls.__name__
+        if name not in _global_connections:
+            _global_connections[name] = []
+        _global_connections[name].append(weakref.ref(instance))
+        return instance
+
+
+class PostGreConnection(metaclass=MetaPostGreConnection):
+    def __init__(self):
+        self._db = None
+
+    def __get__(self, instance, instance_type):
+        if self._db is None:
+            self._db = postgre_connection()
+        return self._db
+
+    def close(self):
+        if self._db is not None:
+            self._db.commit()
+            self._db.close()
+        self._db = None
+
